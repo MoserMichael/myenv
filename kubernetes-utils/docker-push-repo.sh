44af57361dc17e8eb3df7f0f4ea9e6374864d414
MichaelMoser <mmoser@dhcp-3-224.tlv.redhat.com>
2020-01-26 09:31:51 +0200

    put docker/k8s stuff into own directory. add two scripts.

---
+++ kubernetes-utils/docker-push-repo.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+set -x
+
+set 
+
+function Help()
+{
+    if [[ $1 != "" ]]; then
+        echo "Error: $*"
+    fi
+
+	cat <<EOF
+Usage: $0 -u <user> -i <image name to upload>  -n <docker repository name> -r <registry>  [-v] [-h] 
+
+-i <image name to upload>
+-n <docker repository name>
+-r <docker registry>            : defaults to quay.io
+-u <docker registry user>
+
+environment variable DOCKER_REGISTRY_PASSWORD must be set to the docker repository password.
+
+-v : verbose run
+-h : help
+
+Uploads the <image name to upload> to <registry>/<docker repository name> (by default the quay.io registry is used for -r)
+Logs into registry before pushing the password, and logs out after that.
+Uses <user> and environment varialbe DOCKER_REGISTRY_PASSWORD for the password
+
+EOF
+	exit 1
+}
+
+DOCKER_REGISTRY=quay.io
+
+while getopts "hvu:i:n:r:" opt; do
+  case ${opt} in
+    h)
+	Help
+        ;;
+    u)
+        DOCKER_REGISTRY_USER=$OPTARG
+        ;;
+    i)
+        IMAGE_NAME_WITH_TAG=$OPTARG
+        ;;
+    n)
+        DOCKER_REGISTRY_REPOSITORY_NAME=$OPTARG
+        ;;
+    r)
+        DOCKER_REGISTRY=$OPTARG
+        ;;
+    v)
+	set -x
+	export PS4='+(${BASH_SOURCE}:${LINENO})'
+	VERBOSE=1
+        ;; 
+   esac
+done	
+
+if [[ "$IMAGE_NAME_WITH_TAG" == "" ]]; then
+    Help "please set -u <option>"
+fi
+has_image=$(docker images | sed '1d' | awk '{ print $1":"$2 }' |  grep -E "^${IMAGE_NAME_WITH_TAG}$" | wc -l)
+if [[ "$has_image" == "0" ]]; then
+    Help "no local docker image ${IMAGE_NAME_WITH_TAG} exists"
+fi
+
+if [[ "$DOCKER_REGISTRY_USER" == "" ]]; then
+    Help "please set -u option"
+fi
+
+if [ "$DOCKER_REGISTRY_PASSWORD" == "" ]; then
+    Help "please set environment variable DOCKER_REGISTRY_PASSWORD so that script can log into registry"
+fi
+
+if [[ "$DOCKER_REGISTRY" == "" ]]; then
+    Help "please set -r option"
+fi
+
+echo -n "$DOCKER_REGISTRY_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USER" --password-stdin ${DOCKER_REGISTRY}
+
+CONTAINER_SHA=$(docker container create $IMAGE_NAME_WITH_TAG)
+
+docker commit "${CONTAINER_SHA}" ${DOCKER_REGISTRY}/${DOCKER_REGISTRY_USER}/${DOCKER_REGISTRY_REPOSITORY_NAME}
+
+docker push ${DOCKER_REGISTRY}/${DOCKER_REGISTRY_USER}/${DOCKER_REGISTRY_REPOSITORY_NAME}
+
+docker container rm ${CONTAINER_SHA}
+
+docker logout ${DOCKER_REGISTRY} 
+
+echo "*** pushed successfully. remember to set access in repo UI, can't do that from command line ***"
---
+++ kubernetes-utils/pod-logs.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+set -e
+
+KCMD=kubectl
+
+function Help()
+{
+    if [[ "$1" != "" ]]; then
+        echo "Error: $@"
+    fi
+    cat <<EOF
+Usage: $0 -p <podname> [-n <namespace>] [-v] [-h] 
+
+-p : <podname>
+-n : <namespace> (optional)
+-v : verbose run
+-h : help
+
+1) describes the pod ($KCMD describe pod <podname> -n <namespace>)
+2) for each container in the pod template:
+     - show the container definition from the template
+     - show the log for that container as part of the pod
+
+is supposed to help with debugging pod problems
+
+EOF
+    exit 1
+}
+
+while getopts "hvn:p:" opt; do
+  case ${opt} in
+    h)
+	Help
+        ;;
+    n)
+        POD_NSPACE=$OPTARG
+        ;;
+    p)
+        POD_NAME=$OPTARG
+        ;;
+    v)
+	set -x
+	export PS4='+(${BASH_SOURCE}:${LINENO}) '
+	VERBOSE=1
+        ;; 
+   esac
+done	
+
+if [[ "$POD_NSPACE" != "" ]]; then
+  POD_NSPACE_SPEC="-n $POD_NSPACE"
+fi
+
+if [[ "$POD_NAME" == "" ]]; then
+    Help "missing pod name"
+fi
+
+
+
+CMD="$KCMD describe pod $POD_NAME $POD_NSPACE_SPEC"
+cat <<EOF
+
+**** describe pod: $POD_NAME namespace: $POD_NSPACE
+**** command: $CMD
+
+EOF
+
+$CMD
+
+CONTAINER_NAMES=$($KCMD get pod $POD_NAME $POD_NSPACE_SPEC  -o=jsonpath='{.spec.containers[*].name}') 
+
+cat << EOF
+
+*** pod: $POD_NAME namespace: $POD_NAMESPACE containers: ${CONTAINER_NAMES}
+
+EOF
+
+CONTAINER_COUNT=0
+for containername in $CONTAINER_NAMES; do
+    
+    cat <<EOF
+
+**** container spec. container-name: $containername  in pod: $POD_NAME namespace: $POD_NSPACE 
+
+EOF
+    $KCMD get pod $POD_NAME $POD_NSPACE_SPEC -o=json | jq '.spec.containers['"$CONTAINER_COUNT"']'
+
+    CMD="$KCMD logs $POD_NAME $POD_NSPACE_SPEC -c $containername"
+
+    cat <<EOF
+
+**** container logs. container-name: $containername in pod: $POD_NAME namespace: $POD_NSPACE 
+**** command: $CMD
+
+EOF
+    $CMD
+
+    ((CONTAINER_COUNT+=1))
+done
