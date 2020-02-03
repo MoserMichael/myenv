ea600e7c0bd9c42c123401ed0a578ef710dd968b
MichaelMoser <mmoser@localhost.localdomain>
2020-02-03 07:27:15 +0200

    fix setup.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,18 @@
 
 set -x
 
+function install {
+    from="$1"
+    to="$2"
+
+    if [ -f "$to" ]; then
+        cp "$to" "$to.backup-setup-env"
+    fi  
+
+    cp -f "$from" "$to"
+}
+
+
 CNT=$(cat /etc/os-release | grep Fedora | wc -l)
 if [ $CNT != 0 ]; then 
     TYPE=fedora
@@ -9,6 +21,8 @@ else
     echo "unsupported for now"
     exit 1
 fi
+
+
 
 echo "*** install packages ***"
 sudo dnf -y install tmux vim ctags gotags git g++
@@ -22,8 +36,9 @@ git clone https://github.com/tmux-plugins/tmux-resurrect ~/clone/path
 
 git clone https://github.com/tmux-plugins/tmux-continuum ~/clone/path
 
-cp -f .tmux.conf ~/.tmux.conf
-
+install .tmux.conf ~/.tmux.conf
+install .bashrc   ~/.bashrc
+install .vimrc    ~/.vimrc
 
 cat <<EOF
 *** tmux save/restore ***
