ebaf23890928754ac029dda94c9d88fad1404ed8
Michael Moser <mmoser@redhat.com>
2020-03-03 04:07:05 +0200

    change size-of-git-repos into one line
    Signed-off-by: Michael Moser <mmoser@redhat.com>

--- size-of-git-repos.sh
+++ size-of-git-repos.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # finds all git repositories under the curren directory and sorts them by disk space they consume
+find . -name '.git' -type 'd' -print0 | xargs -0  du -sh | sort -h -k 1
 
-REPO_DIRS=$(find . -name '.git' -type 'd')
-
-OUT=""
-
-while IFS= read -r line; do 
-    dir=$(dirname "$line")
-    OUT="$OUT"'\n'$(du -sh "$dir")
-done <<< "$REPO_DIRS"
-
-echo -e "$OUT" | sort -h -k 1
+#REPO_DIRS=$(find . -name '.git' -type 'd')
+#
+#OUT=""
+#
+#while IFS= read -r line; do 
+#    dir=$(dirname "$line")
+#    OUT="$OUT"'\n'$(du -sh "$dir")
+#done <<< "$REPO_DIRS"
+#
+#echo -e "$OUT" | sort -h -k 1
