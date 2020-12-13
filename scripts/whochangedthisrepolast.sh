d9c548ab595e4afe032d21138a332f0b3cfa50f4
Michael Moser <moser.michael@gmail.com>
2020-12-13 17:39:25 +0200

    add two scripts.

---
+++ scripts/whochangedthisrepolast.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+if [[ $1 == "-h" ]]; then
+    if [[ ! -z "${SHORT_HELP_MODE}" ]]; then
+        echo "for each files in git repo in current directory - show who made the last revision."
+        exit 1
+    fi
+
+cat <<EOF
+$0 
+
+for each files in current git repo - show who made the last revision.
+also show a summary of how many files were modified last per user
+
+EOF
+exit 1
+fi
+
+
+# find the top level directory for this git repository
+TOP_DIR=`git rev-parse --show-toplevel 2>/dev/null`
+if [ "x$TOP_DIR" != "x" ]; then
+    cd $TOP_DIR
+else 
+    echo "$PWD is not a git repo"
+    exit 1
+fi
+
+FILES=$(git ls-files)
+
+for f in $FILES; do
+
+    who_wrote_this=$(git log --pretty='%ae %ad' $f | head -n 1)
+    
+    echo "$f :: $who_wrote_this"
+done
+
+
+WHO=""
+for f in $FILES; do
+
+    who_wrote_this=$(git log --pretty='%ae' $f | head -n 1)
+
+    WHO=$(echo -e "$WHO\n$who_wrote_this")
+
+done  
+
+echo ""
+echo "summary:"
+echo "$WHO" | tail -n +2 |  sort | uniq -c  
+
+
+
+
+
+
